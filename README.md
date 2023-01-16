257f4ece2e94c21066a5d8d3ee0309f8f2b924d8
MikeDuell <5duelm42@solent.ac.uk>
2023-01-16 13:03:57 +0000

    added login and logout functionality

--- README.md
+++ README.md
@@ -11,5 +11,5 @@
 
 
 
-- If this is your first time getting started then run the seeder - `node seeder`
+
 - Run the project in development mode - `npm run dev`
--- app.js
+++ app.js
@@ -6,12 +6,44 @@ const app = express();
 const mongoose = require("mongoose");
 const { WEB_PORT, MONGODB_URI } = process.env;
 const bodyParser = require("body-parser");
+const expressSession = require("express-session");
+app.use(expressSession({ secret: 'foo barr', cookie: { expires: new Date(253402300000000) } }))
 
+global.user = false
+app.use('*', async(req, res, next) => {
+  if (req.session.userID && !global.user) {
+    const user = await User.findById(req.session.userID);
+    global.user = user
+  }
+  next();
+  })
+
+  app.get("/logout", async (req, res) => {
+    req.session.destroy();
+    global.user = false;
+    res.redirect('/');
+  })
 
 // middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}))
+
+// disable add products if there is no user logged in
+
+const authMiddleware = async (req, res, next) => {
+  const user = await User.findById(req.session.userID);
+  if (!user) {
+    return res.redirect('/');
+  }
+  next()
+}
+app.get("/create-adherence", authMiddleware, (req, res) => {
+  res.render("create-adherence", { errors: {} });
+});
+app.get("/create-stirtank", authMiddleware, (req, res) => {
+  res.render("create-stirtank", { errors: {} });
+});
 
 // mongodb connection
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
@@ -26,33 +58,35 @@ app.set("view engine", "ejs");
 // route handlers
 
 const adherenceController = require("./controllers/adherence");
-app.get("/adherences", adherenceController.list)
+app.get("/adherences", authMiddleware, adherenceController.list)
 app.get("/adherences/delete/:id", adherenceController.delete)
 app.post("/create-adherence", adherenceController.create);
 app.get("/adherences/update/:id", adherenceController.edit);
 app.post("/adherences/update/:id", adherenceController.update);
 
 const customerController = require("./controllers/customer");
-app.get("/customers", customerController.list)
+app.get("/customers", authMiddleware,  customerController.list)
 app.get("/customers/delete/:id", customerController.delete)
 
 const stirtankController = require("./controllers/stirtank");
-app.get("/stirtanks", stirtankController.list)
+app.get("/stirtanks", authMiddleware, stirtankController.list)
 app.get("/stirtanks/delete/:id", stirtankController.delete)
 app.post("/create-stirtank", stirtankController.create);
 app.get("/stirtanks/update/:id", stirtankController.edit);
 app.post("/stirtanks/update/:id", stirtankController.update);
 
-/**
- * notice above we are using dotenv. We can now pull the values from our environment
- */
+const userController = require("./controllers/user");
+const User = require("./models/User");
+app.get("/join", (req, res) => {
+  res.render('create-user', { errors: {} })
+});
+app.post("/join", userController.create);
 
+app.get("/signin", (req, res) => {
+  res.render('signin', { errors: {} })
+});
+app.post("/signin", userController.login);
 
-
-
-
- 
- 
 
 
 
@@ -79,9 +113,7 @@ app.get("/create-stirtank", (req, res) => {
 app.get("/create-adherence", (req, res) => {
   res.render("create-adherence", {errors: {}})
 });
-app.get("/create-user", (req, res) => {
-  res.render("create-user")
-});
+
 
 
 app.listen(WEB_PORT, () => {
